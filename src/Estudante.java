9d1d3cc60a75caa25d95d0be5e3c8357227d87c6
DaviKM <davikmach@gmail.com>
2026-08-03 16:35:04 -0300

    criação e testes da classe Estudante

---
+++ src/Estudante.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Estudante {
+        String nome;
+        double[] notas;
+
+        public Estudante(String nome) {
+            this.nome = nome;
+            this.notas = new double[5];
+        }
+
+        public void insereNotas(){
+            Scanner sc = new Scanner(System.in);
+            for(int i = 0; i<this.notas.length; i++){
+                System.out.printf("Insira a %dª nota:\t", i + 1);
+                this.notas[i] = sc.nextDouble();
+            }
+        }
+
+        public double calculaMedia(){
+            double total = 0;
+            for (double nota : this.notas) {
+                total = total + nota;
+            }
+            return total/this.notas.length;
+        }
+
+        public double menorNota(){
+            double menor = this.notas[0];
+            for (double nota : this.notas) {
+                if (nota < menor){
+                    menor = nota;
+                }
+            }
+            return menor;
+        }
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
