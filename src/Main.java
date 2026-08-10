b95e6dc9af451f29c40b056bb4485856d145afb2
DaviKM <davikmach@gmail.com>
2026-08-10 16:40:59 -0300

    testes no main ainda não funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Estudante e = new Estudante("Davi");
-        e.insereNotas();
-        System.out.println(e.calculaMedia());
-        System.out.println(e.menorNota());
-
+        Estudante[] e = new Estudante[3];
+        for (Estudante estudante : e) {
+            estudante = new Estudante("A");
+            estudante.insereNotas();
+        }
+        System.out.println(Estudante.aprovados(e));
     }
 }
